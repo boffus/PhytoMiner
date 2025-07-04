aed12707c609544bf31134b309d06747ae9fc70c
Kris Kari <toffekari@gmail.com>
2025-07-05 02:49:56 +1000

    Create test1.py
    A placeholder test

---
+++ tests/test1.py
@@ -0,0 +1,7 @@
+def test_ci_pipeline_works():
+    """
+    A placeholder test to ensure the pytest setup in the CI pipeline works.
+    This test will be removed or replaced as real tests are added.
+    """
+    assert True
+
