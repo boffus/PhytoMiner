2031191c76415d332e232d5752275ba5e4e0bf02
Kris Kari <toffekari@gmail.com>
2025-07-05 03:04:45 +1000

    Update and rename test1.py to test_processing.py

--- tests/test1.py
+++
@@ -1,7 +0,0 @@
-def test_ci_pipeline_works():
-    """
-    A placeholder test to ensure the pytest setup in the CI pipeline works.
-    This test will be removed or replaced as real tests are added.
-    """
-    assert True
-
